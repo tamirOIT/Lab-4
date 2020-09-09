a74b05c4645e04e154549ff941c4d89b41f0d7ef
Mari Good <goodm@lanecc.edu>
2020-09-09 13:38:31 -0700

    Added starting files

---
+++ MMABooksEFCore/MMABooksTests/StateTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    public class StateTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
